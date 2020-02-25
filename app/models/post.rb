2ece717799c40ebffbbf2aed640870838bc56e3d
hiro266 <t1freejob@gmail.com>
2020-02-25 23:55:52 +0900

    Fix: titleの最大文字数を変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  
-  validates :title, presence: true, length: { maximum: 50 }
+
+  validates :title, presence: true, length: { maximum: 100 }
   validates :body, length: { maximum: 65_535 }
   validates :youtube_url, presence: true, length: { maximum: 100 }
 end
