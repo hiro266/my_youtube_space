97ea9f0c47d795e10e94a88c48e001b05b0e8c75
hiro266 <t1freejob@gmail.com>
2020-02-25 17:42:09 +0900

    Add: commentsテーブルとのアソシエーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, length: { maximum: 65_535 }
   validates :youtube_url, presence: true, length: { maximum: 100 }
