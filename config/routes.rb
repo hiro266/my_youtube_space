d4b2c4ffbe76cbec0d4bc195de621a52950b04c9
hiro266 <t1freejob@gmail.com>
2020-03-02 16:46:00 +0900

    Add: ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :comments, only: %i[create edit update destroy]
   end
   resource :profile, only: %i[show edit update]
+  resources :password_resets, only: %i[new create edit update]
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   get    '/login',  to: 'user_sessions#new'
   post   '/login',  to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
