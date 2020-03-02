4a072ad0255d3b9db62329b4cf3499b32ec31c91
hiro266 <t1freejob@gmail.com>
2020-03-02 17:58:44 +0900

    Add: postsテーブルbodyカラムを削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,6 @@ class Post < ApplicationRecord
   has_many :tags, through: :tagmaps
 
   validates :title, presence: true, length: { maximum: 100 }
-  validates :body, length: { maximum: 65_535 }
   validates :youtube_url, presence: true, length: { maximum: 100 }
 
   def save_posts(tags)
